572547d086fde59fc935dc9e4606a2f3d0a69bca
Jon Burns <jonburns10@gmail.com>
2017-04-19 15:16:06 -0700

    added rutta and ruttafying

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-	// var button = document.getElementById('download-btn');
-	var textarea = document.getElementsByTagName('textarea');
-	function getTextarea () {
-		for (var i = 0; i < textarea.length; i++) {
-			console.log(textarea[i]);
-			return textarea[i];
-		}
-	}
-	getTextarea();
-	// textarea[i].addEventListener('keypress', function() {
-	// 	if (this.scrollTop != 0){
-	// 		console.log(this.scrollTop);
-	// 	   this.style.height = this.scrollHeight + "px";
-	// 	}
-	// });
 
+	// var button = document.getElementById('download-btn');
+	// function savePDF () {
+	// 	var pdf = new jsPDF();
+	// 	pdf.save(window.location.pathname + '.pdf');
+	// }
+	// button.addEventListener('click', function(){
+	// 	savePDF();
+	// 	console.log("clic");
+	// });
+	// // var button = document.getElementById('download-btn');
+	// var textarea = document.getElementsByTagName('textarea');
+	// function getTextarea () {
+	// 	for (var i = 0; i < textarea.length; i++) {
+	// 		console.log(textarea[i]);
+	// 		return textarea[i];
+	// 	}
+	// }
+	// getTextarea();
+	// // textarea[i].addEventListener('keypress', function() {
+	// // 	if (this.scrollTop != 0){
+	// // 		console.log(this.scrollTop);
+	// // 	   this.style.height = this.scrollHeight + "px";
+	// // 	}
+	// // });
+	//
 
 	console.log('Loaded');
 });
