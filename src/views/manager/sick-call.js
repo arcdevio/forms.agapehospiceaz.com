933a96dce490955d8ca48eb22bade483b8d3c070
Jon Burns <jonburns10@gmail.com>
2017-10-17 16:01:05 -0700

    added sick call

---
+++ src/views/manager/sick-call.js
@@ -0,0 +1,6 @@
+Jenie.component({
+	name: 'sick-call',
+	template: `
+		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfjiZbFCIvJDRhE4412Ilojkz_m5FbG39BvXjJg01KudrVwaQ/viewform?embedded=true" frameborder="0">Loading...</iframe>
+	`
+});
