4a408836f5d17b3672b37961ef0c1b35f8dc00e9
Jon Burns <jonburns10@gmail.com>
2019-01-03 11:53:04 -0700

    update to incident report forms

--- docs/views/incident-report.js
+++ docs/views/incident-report.js
@@ -1,6 +1,6 @@
 Oxe.component.define({
 	name: 'incident-report',
 	html: `
-		<iframe src="https://docs.google.com/a/agapehospiceaz.com/forms/d/e/1FAIpQLSeKWnzhl2OPHydCIP2lGL0OGsfiqqMR6uBBA11CVjlIFhcAdQ/viewform?embedded=true" frameborder="0"></iframe>
+		<iframe src="https://docs.google.com/a/agapehospiceaz.com/forms/d/e/1FAIpQLScrGlDHAKeHIPDSJw1ABrc7nKE2VuKwvsBtu-GnAWDSHnmOyg/viewform?embedded=true" frameborder="0"></iframe>
 	`
 });
--- docs/views/manager/incident-investigation.js
+++ docs/views/manager/incident-investigation.js
@@ -1,6 +1,6 @@
 Oxe.component.define({
 	name: 'manager-incident-investigation',
 	html: `
-		<iframe src="https://docs.google.com/a/agapehospiceaz.com/forms/d/e/1FAIpQLSdza_Zaxr45Jem4xnxF3738SaFVjOo7WtLJ54NpBJVqFz1oAQ/viewform?embedded=true" frameborder="0"></iframe>
+		<iframe src="https://docs.google.com/a/agapehospiceaz.com/forms/d/e/1FAIpQLSetSW0HlgzGwn7D1i0o4H4vys46BNrw73PhYlMCX9iKL8_DAg/viewform?embedded=true" frameborder="0"></iframe>
 	`
 });
